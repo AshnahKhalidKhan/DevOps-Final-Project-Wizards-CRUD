577a4dfe58bd6b4e5c015f802794e3402d31adde
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2024-05-11 16:06:58 +0500

    Changed IP

--- backend/constants/constants.js
+++ backend/constants/constants.js
@@ -1,5 +1,5 @@
 export const BACKEND_PORT = 5000;
-export const BACK_URL = 'http://localhost:5000';
+export const BACK_URL = 'http://34.128.89.226:5000';
 export const FRONTEND_IP = 'http://localhost:5173';
 export const JSON_FILE_PATH = 'data/wizards.json';
 export const dbURI =
