9824e4710f647448fad04780eb73bc9a823307e1
AshnahKhalidKhan <a.khan.22889@khi.iba.edu.pk>
2024-05-11 15:40:15 +0500

    Testing backend with Jest and Babel

--- backend/models/wizard.test.js
+++ backend/models/wizard.test.js
@@ -1,29 +1,46 @@
-import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import { Wizard } from './wizard'; // Adjust path as necessary
+import mongoose from "mongoose";
+import { Wizard } from "./wizard"; // adjust the path to where your model is actually defined
 
-let mongoServer;
+describe('Wizard Model Test', () => {
+    beforeAll(async () => {
+        const url = 'mongodb://127.0.0.1/wizardTest';
+        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    });
 
-beforeAll(async () => {
-    mongoServer = await MongoMemoryServer.create();
-    const uri = mongoServer.getUri();
-    await mongoose.connect(uri);
-});
+    afterAll(async () => {
+        await mongoose.connection.dropDatabase();
+        await mongoose.connection.close();
+    });
 
-afterAll(async () => {
-    await mongoose.disconnect();
-    await mongoServer.stop();
-});
+    test('create & save wizard successfully', async () => {
+        const validWizard = new Wizard({ name: 'Gandalf', age: 2019, imagePath: 'path/to/image.jpg' });
+        const savedWizard = await validWizard.save();
 
-describe('Wizard model', () => {
-    it('should save a wizard', async () => {
-        const wizardData = { name: 'Merlin', age: 300, imagePath: 'backend\\uploads\\1704369644544.jpeg' };
-        const wizard = new Wizard(wizardData);
-        const savedWizard = await wizard.save();
-
+        // Object Id should be defined when successfully saved to MongoDB.
         expect(savedWizard._id).toBeDefined();
-        expect(savedWizard.name).toBe(wizardData.name);
-        expect(savedWizard.age).toBe(wizardData.age);
-        expect(savedWizard.imagePath).toBe(wizardData.imagePath);
+        expect(savedWizard.name).toBe('Gandalf');
+        expect(savedWizard.age).toBe(2019);
+        expect(savedWizard.imagePath).toBe('path/to/image.jpg');
+    });
+
+    test('insert wizard successfully, but the field does not defined in schema should be undefined', async () => {
+        const wizardWithInvalidField = new Wizard({ name: 'Gandalf', age: 2019, imagePath: 'path/to/image.jpg', power: 500 });
+        const savedWizardWithInvalidField = await wizardWithInvalidField.save();
+        expect(savedWizardWithInvalidField._id).toBeDefined();
+        expect(savedWizardWithInvalidField.power).toBeUndefined();
+    });
+
+    test('create wizard without required field should failed', async () => {
+        const wizardWithoutRequiredField = new Wizard({ name: 'Gandalf' });
+        let err;
+        try {
+            const savedWizardWithoutRequiredField = await wizardWithoutRequiredField.save();
+            error = savedWizardWithoutRequiredField;
+        } catch (error) {
+            err = error;
+        }
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+        expect(err.errors.age).toBeDefined();
+        expect(err.errors.imagePath).toBeDefined();
     });
 });
